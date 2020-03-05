1cceda8354225f5ab2900f7f210b05a7749f362c
Plorence <ploseok@gmail.com>
2020-03-05 21:48:07 +0900

    Add MCodeGeneratorOptions class member

--- MCodeGenerator.cs
+++ MCodeGenerator.cs
@@ -14,6 +14,7 @@ namespace mcode_generator
     {
         public bool _IsX86 { get; set; }
         public bool _IsX64 { get; set; }
+        public string OptimizationLevel { get; set; }
     }
     public class MCodeGenerator : IDisposable
     {
